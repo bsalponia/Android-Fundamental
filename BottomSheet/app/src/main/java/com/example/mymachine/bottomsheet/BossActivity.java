2baa7bf1c85ed64a0f470bb9559e2adf289123aa
bsalponia <bhriguraj@beusalons.com>
2016-11-16 10:48:06 +0530

    initial commit

---
+++ BottomSheet/app/src/main/java/com/example/mymachine/bottomsheet/BossActivity.java
@@ -0,0 +1,48 @@
+package com.example.mymachine.bottomsheet;
+
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BossActivity extends AppCompatActivity {
+
+    BottomSheetBehavior bottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss);
+
+        View bottomSheet= findViewById(R.id.bottomSheet);
+        bottomSheetBehavior= BottomSheetBehavior.from(bottomSheet);
+
+        Button button= (Button)findViewById(R.id.btnmain);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_COLLAPSED) {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
